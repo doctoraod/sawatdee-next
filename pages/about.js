7bd014369bc31d0da3965bfc2ecc57cfb2a4f0ae
Suvichan Prasongphonchai <suvichan_pra@sendit.asia>
2017-08-21 01:02:36 +0700

    Add lesson 2

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,25 @@
-import Link from 'next/link'
+import Router from 'next/router'
+
+const handleClickIndex = () => Router.push({
+  pathname: '/'
+})
+
+const style = {
+  backgroundColor: '#4CAF50',
+  border: 'none',
+  color: 'white',
+  padding: '10px 10px',
+  textAlign: 'center',
+  textDecoration: 'none',
+  display: 'inline-block',
+  fontSize: '16px',
+  margin: '4px 2px',
+  cursor: 'pointer',
+}
 
 const Index = () => (
   <div>
-    <Link href="/">
-      <a>Index Page</a>
-    </Link>
+    <a onClick={() => handleClickIndex()} style={style}>Index Page</a>
     <h1>About</h1>
   </div>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
-import Link from 'next/link'
+import Router from 'next/router'
+
+const handleClickAbout = () => Router.push({
+  pathname: '/about'
+})
+
+const style = {
+  backgroundColor: '#4CAF50',
+  border: 'none',
+  color: 'white',
+  padding: '10px 10px',
+  textAlign: 'center',
+  textDecoration: 'none',
+  display: 'inline-block',
+  fontSize: '16px',
+  margin: '4px 2px',
+  cursor: 'pointer',
+}
 
 const Index = () => (
   <div>
-    <Link href="/about">
-      <a>About Page</a>
-    </Link>
+    <a onClick={() => handleClickAbout()} style={style}>About Page</a>
     <h1>Sawatdee Next.js</h1>
     <p>Index</p>
   </div>
